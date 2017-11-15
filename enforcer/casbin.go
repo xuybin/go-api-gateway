263ef3694fe3f0c86aacbb277783bfdc91577cce
xuybin <xuybin@qq.com>
2017-11-15 20:28:08 +0800

    [feature]support create database if not exists

--- enforcer/casbin.go
+++ enforcer/casbin.go
@@ -38,24 +38,3 @@ func createMysqlDatabase( dataSourceName string) (err error) {
 	}
 	return
 }
-
-func SubString(str string,begin,length int) (substr string) {
-	// 将字符串的转换成[]rune
-	rs := []rune(str)
-	lth := len(rs)
-
-	// 简单的越界判断
-	if begin < 0 {
-		begin = 0
-	}
-	if begin >= lth {
-		begin = lth
-	}
-	end := begin + length
-	if end > lth {
-		end = lth
-	}
-
-	// 返回子串
-	return string(rs[begin:end])
-}
